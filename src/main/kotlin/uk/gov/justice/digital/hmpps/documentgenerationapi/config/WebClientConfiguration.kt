e0dc826ce8bf1b446ee61ddec53385db4649f931
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2026-02-13 09:39:10 +0000

    MIA-1380 initial project setup

--- src/main/kotlin/uk/gov/justice/digital/hmpps/documentgenerationapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/documentgenerationapi/config/WebClientConfiguration.kt
@@ -9,9 +9,9 @@ import java.time.Duration
 
 @Configuration
 class WebClientConfiguration(
-  @param:Value("\${hmpps-auth.url}") val hmppsAuthBaseUri: String,
-  @param:Value("\${api.health-timeout:2s}") val healthTimeout: Duration,
-  @param:Value("\${api.timeout:20s}") val timeout: Duration,
+  @Value($$"${integration.hmpps-auth.url}") private val hmppsAuthBaseUri: String,
+  @Value("\${api.health-timeout:1s}") val healthTimeout: Duration,
+  @Value("\${api.timeout:1s}") val timeout: Duration,
 ) {
   // HMPPS Auth health ping is required if your service calls HMPPS Auth to get a token to call other services
   @Bean
