0993a21157e288b1b2149f54ce92d09ea4888444
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2026-02-26 17:35:09 +0000

    Update swagger doc schemas (#9)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/documentgenerationapi/model/TemplateRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/documentgenerationapi/model/TemplateRequest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.documentgenerationapi.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import io.swagger.v3.oas.annotations.media.Schema
 import java.util.UUID
 
 data class TemplateRequest(
@@ -14,7 +15,10 @@ data class TemplateRequest(
   @JsonIgnore
   val type = DocumentType.DOCUMENT_GENERATION_TEMPLATES
 
+  @Schema(name = "TemplateRequest.Variable")
   data class Variable(val code: String, val required: Boolean)
+
+  @Schema(name = "TemplateRequest.Group")
   data class Group(val code: String)
 }
 
--- src/main/kotlin/uk/gov/justice/digital/hmpps/documentgenerationapi/model/TemplateVariables.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/documentgenerationapi/model/TemplateVariables.kt
@@ -1,8 +1,12 @@
 package uk.gov.justice.digital.hmpps.documentgenerationapi.model
 
+import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.documentgenerationapi.domain.TemplateVariable
 
 data class TemplateVariables(val domains: List<Domain>) {
+  @Schema(name = "TemplateVariables.Domain")
   data class Domain(val code: String, val description: String, val variables: List<Variable>)
+
+  @Schema(name = "TemplateVariables.Variable")
   data class Variable(val code: String, val description: String, val type: TemplateVariable.Type)
 }
