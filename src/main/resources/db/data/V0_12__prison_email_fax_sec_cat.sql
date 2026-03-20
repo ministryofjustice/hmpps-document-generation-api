4f025762f09df4c7a518b20c7c021703fe96f83b
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2026-03-20 09:17:46 +0000

    MIA-1737 new template variables for prison email/fax and sec cat (#35)

---
+++ src/main/resources/db/data/V0_12__prison_email_fax_sec_cat.sql
@@ -0,0 +1,4 @@
+insert into template_variable(domain, code, description, sequence_number, type)
+values ('PRISON', 'prsnEmailFax', 'Prison email or fax', 150, 'STRING'),
+       ('PRISON', 'prsnSecCat', 'Prison security category', 160, 'STRING')
+;
