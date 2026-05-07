4b92846934c82cfbaa480cbc18219fdb7697601a
Anthony Britton <anthony.britton@justice.gov.uk>
2026-05-07 10:43:52 +0100

    MIA-1848

--- src/main/kotlin/uk/gov/justice/digital/hmpps/documentgenerationapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/documentgenerationapi/config/WebClientConfiguration.kt
@@ -4,43 +4,59 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClient.Builder
-import uk.gov.justice.hmpps.kotlin.auth.authorisedWebClient
-import uk.gov.justice.hmpps.kotlin.auth.healthWebClient
+import reactor.netty.http.client.HttpClient
 import java.time.Duration
+import java.time.Duration.ofSeconds
 
 @Configuration
 class WebClientConfiguration(
-  @Value($$"${integration.hmpps-auth.url}") private val hmppsAuthBaseUri: String,
+  private val serviceConfig: ServiceConfig,
   @Value($$"${integration.document-management.url}") private val documentManagementBaseUri: String,
   @Value($$"${integration.manage-users.url}") private val manageUsersBaseUri: String,
   @Value($$"${integration.template-configuration.url:}") private val templateConfigurationBaseUri: String,
-  @Value($$"${api.health-timeout:1s}") val healthTimeout: Duration,
-  @Value($$"${api.timeout:1s}") val timeout: Duration,
 ) {
-  // HMPPS Auth health ping is required if your service calls HMPPS Auth to get a token to call other services
-  @Bean
-  fun hmppsAuthHealthWebClient(builder: Builder): WebClient = builder.healthWebClient(hmppsAuthBaseUri, healthTimeout)
 
   @Bean
-  fun documentManagementHealthWebClient(builder: Builder): WebClient = builder.healthWebClient(documentManagementBaseUri, healthTimeout)
+  fun documentManagementWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: Builder) = builder.authorisedWebClient(documentManagementBaseUri, authorizedClientManager)
 
   @Bean
-  fun manageUsersHealthWebClient(builder: Builder): WebClient = builder.healthWebClient(manageUsersBaseUri, healthTimeout)
-
-  @Bean
-  fun documentManagementWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: Builder) = builder.authorisedWebClient(authorizedClientManager, DEFAULT_REGISTRATION_ID, documentManagementBaseUri, timeout)
-
-  @Bean
-  fun manageUsersWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: Builder) = builder.authorisedWebClient(authorizedClientManager, DEFAULT_REGISTRATION_ID, manageUsersBaseUri, timeout)
+  fun manageUsersWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: Builder) = builder.authorisedWebClient(manageUsersBaseUri, authorizedClientManager)
 
   @Bean
   @ConditionalOnBooleanProperty("service.allow-pulling-templates")
-  fun templateConfigurationWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: Builder) = builder.authorisedWebClient(authorizedClientManager, "template-config", templateConfigurationBaseUri, timeout)
+  fun templateConfigurationWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: Builder) = builder.authorisedWebClient(
+    templateConfigurationBaseUri,
+    authorizedClientManager,
+    registrationId = "template-config",
+  )
+
+  fun Builder.authorisedWebClient(
+    url: String,
+    authorizedClientManager: OAuth2AuthorizedClientManager,
+    timeout: Duration = Companion.timeout,
+    registrationId: String = DEFAULT_REGISTRATION_ID,
+  ): WebClient {
+    val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager).also {
+      it.setDefaultClientRegistrationId(registrationId)
+    }
+
+    return baseUrl(url)
+      .clientConnector(
+        ReactorClientHttpConnector(
+          HttpClient.create().protocol(*serviceConfig.httpProtocol.toTypedArray()).responseTimeout(timeout),
+        ),
+      )
+      .filter(oauth2Client)
+      .build()
+  }
 
   companion object {
     const val DEFAULT_REGISTRATION_ID = "default"
+    private val timeout: Duration = ofSeconds(1)
   }
 }
